cd204b8e74e13ea3f02437959934df05ed9689c7
Amos Barbato <amosbarbato@gmail.com>
2024-11-26 16:44:21 -0300

    feat: add stripe webhooks that handles subscriptions

--- app/subscription/_components/acquire-plan-button.tsx
+++ app/subscription/_components/acquire-plan-button.tsx
@@ -2,8 +2,12 @@
 import { Button } from "@/app/_components/ui/button"
 import { StripeCheckout } from "../_actions/stripe-checkout"
 import { loadStripe } from "@stripe/stripe-js"
+import { useUser } from "@clerk/nextjs"
+import Link from "next/link"
 
 const AcquirePlanButton = () => {
+  const { user } = useUser()
+
   const handleAcquirePlan = async () => {
     const { sessionId } = await StripeCheckout()
     if (!process.env.NEXT_PUBLIC_STRIPE_PUBLISHABLE_KEY) {
@@ -16,6 +20,22 @@ const AcquirePlanButton = () => {
       throw new Error("Stripe not found");
     }
     await stripe.redirectToCheckout({ sessionId })
+  }
+
+  const hasPremiumPlan = user?.publicMetadata.subscriptionPlan == "premium"
+  if (hasPremiumPlan) {
+    return (
+      <Button
+        className="w-full border-primary rounded-full"
+        variant="link"
+      >
+        <Link
+          href={`${process.env.NEXT_PUBLIC_STRIPE_CUSTOMER_PORTAL_URL as string}?prefilled_email=${user.emailAddresses[0].emailAddress}`}
+        >
+          Gerenciar plano
+        </Link>
+      </Button>
+    )
   }
 
   return (
--- app/subscription/page.tsx
+++ app/subscription/page.tsx
@@ -1,10 +1,11 @@
-import { auth } from "@clerk/nextjs/server"
+import { auth, clerkClient } from "@clerk/nextjs/server"
 import Navbar from "../_components/navbar"
 import { redirect } from "next/navigation"
 import { Card, CardContent, CardHeader } from "../_components/ui/card"
 import { CheckIcon, XIcon } from "lucide-react"
 import { Button } from "../_components/ui/button"
 import AcquirePlanButton from "./_components/acquire-plan-button"
+import { Badge } from "../_components/ui/badge"
 
 const Subscription = async () => {
   const { userId } = await auth()
@@ -12,6 +13,9 @@ const Subscription = async () => {
   if (!userId) {
     redirect("/login")
   }
+
+  const { user } = (await clerkClient()).users.getUser(userId)
+  const hasPremiumPlan = user?.publicMetadata.subscriptionPlan == "premium"
 
   return (
     <>
@@ -22,7 +26,7 @@ const Subscription = async () => {
 
         <div className="flex gap-6">
           <Card className="w-[450px]">
-            <CardHeader className="border-b border-solid py-8 space-y-4">
+            <CardHeader className="border-b border-solid py-8 space-y-4 relative">
               <h2 className="text-center text-2xl font-semibold">Plano Básico</h2>
 
               <div className="flex items-center justify-center gap-3">
@@ -57,7 +61,10 @@ const Subscription = async () => {
           </Card>
 
           <Card className="w-[450px]">
-            <CardHeader className="border-b border-solid py-8 space-y-4">
+            <CardHeader className="border-b border-solid py-8 space-y-4 relative">
+              {hasPremiumPlan && (
+                <Badge className="absolute top-12 left-4 bg-primary/10 text-primary">Mais Popular</Badge>
+              )}
               <h2 className="text-center text-2xl font-semibold">Plano Pro</h2>
 
               <div className="flex items-center justify-center gap-3">
